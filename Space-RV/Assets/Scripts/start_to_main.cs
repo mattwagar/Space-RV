793032814ccb67c467d5fd9987c06ecf86d4afd6
Zachary McKenna <zackmckenna@gmail.com>
2019-01-27 17:52:11 -0500

    Start Scene Script

---
+++ Space-RV/Assets/Scripts/start_to_main.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class start_to_main : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        // Only specifying the sceneName or sceneBuildIndex will load the Scene with the Single mode
+        if (Input.GetKeyDown("Fire1")) {
+            SceneManager.LoadScene("Main", LoadSceneMode.Additive);
+      
+    }
+}
+}
